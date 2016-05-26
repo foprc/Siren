528bc6e7c625597bcd077e784a520398bb017f03
熊猫饼饼 <quyizhi@wepiao.com>
2016-05-26 18:10:17 +0800

    first commit

---
+++ Siren/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Siren
+//
+//  Created by 熊猫饼饼 on 5/26/16.
+//  Copyright © 2016 foprc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
